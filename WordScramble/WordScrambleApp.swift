38dce6526212af61c994c627f3562e3590a45cbd
seahpark247 <famousjedo@gmail.com>
2025-03-05 05:34:08 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Seah Park on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Seah Park on 3/5/25.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
